612e48c434aecaa1bba18be46564ec0734a10b31
nick <11andriianovnikita@gmail.com>
2026-02-04 19:34:43 +0300

    обновил CORS

--- transport/middleware.go
+++ transport/middleware.go
@@ -2,17 +2,35 @@ package transport
 
 import "net/http"
 
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173": true,
+}
+
+var allowedMethods = "GET, POST, OPTIONS"
+var allowedHeaders = "Content-Type, Authorization, Accept"
+
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		origin := r.Header.Get("Origin")
+		if isOriginAllowed(origin) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Vary", "Origin")
+		}
+
+		w.Header().Set("Access-Control-Allow-Methods", allowedMethods)
+		w.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func isOriginAllowed(origin string) bool {
+	return allowedOrigins[origin]
 }
